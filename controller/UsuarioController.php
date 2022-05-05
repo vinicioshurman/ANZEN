e5c445d7e383274a6cddb911330f73c521b6fe70
Nathanaely <93411156+nathanaelymendes@users.noreply.github.com>
2022-05-05 15:58:35 -0300

    Add files via upload

---
+++ controller/UsuarioController.php
@@ -0,0 +1,29 @@
+<?php
+require_once './model/Usuario.php';
+
+class UsuarioController{
+    public static function salvar(){
+       
+        $usuario = new Usuario();
+        $usuario->setLogin($_POST['login']);
+        $usuario->setSenha($_POST['senha1']);
+        $usuario->save();
+    }
+
+    public static function editar($id){
+        $usuario = new Usuario;
+        $usuario = $usuario->find($id);
+        return $usuario;
+    }
+
+    public static function listar(){
+        $usuarios = new Usuario;
+        return $usuarios->listAll();
+    }
+    public static function excluir($id)
+    {
+        $usuario = new Usuario;
+        $usuario = $usuario-> remove($id);
+    }
+}
+?>
