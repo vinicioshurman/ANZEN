6886ed4445089ae4d660b54e73704e0daf49a650
Vinicio Shurman <48026024+vinicioShurman@users.noreply.github.com>
2022-05-30 14:25:29 -0300

    adicionando arquivos frontend

---
+++ view/principal.php
@@ -0,0 +1,142 @@
+    <main>
+        <section class="principal">
+            <div class="flex-box-main">
+                <div class="flex-group-0">
+                  <p>Saldo Final</p>
+                  <h1 class="valorGrande">2.000,00</h1>
+                  <p>Saldo Inicial: <strong>2.000,00</strong></p>
+                  <p>Mês de Janeiro</p>
+                </div>
+                <div class="flex-group-1">
+                  <label for="">Teste1</label><br>
+                  <input type="text">
+
+                  <br>
+
+                  <label for="">Teste2</label><br>
+                  <input type="text" class="noMargin">
+                </div>
+                <div class="flex-group-2">
+                  <label for="">Teste3</label><br>
+                  <input type="text">
+                  
+                  <br>
+
+                  <label for="">Teste4</label><br>
+                  <input type="text" class="noMargin">
+                </div>
+                <div class="flex-group-3">
+                  <label for="">Teste5</label><br>
+                  <div class="flex-input-button">
+                    <div style="flex-grow: 1">
+                      <button class="more-less">+</button>
+                    </div>
+                    <div style="flex-grow: 8">
+                      <input type="text">
+                    </div>
+                  </div>
+              
+                  <br>
+
+                  <button class="btn btn-color-green btn-adicionar">Adicionar</button>
+                </div>
+
+            </div>
+            <div class="tabela-principal">
+              <table cellpadding="0" cellspacing="0">
+                <tr>
+                  <th>Data</th>
+                  <th>Descrição</th>
+                  <th>Forma de pagamento</th>
+                  <th>Categoria</th>
+                  <th>Quantidade</th>
+                </tr>
+                <tr>
+                  <td>teste1</td>
+                  <td>teste1</td>
+                  <td>teste1</td>
+                  <td>teste1</td>
+                  <td>teste1</td>
+                </tr>
+                <tr>
+                  <td>teste2</td>
+                  <td>teste2</td>
+                  <td>teste2</td>
+                  <td>teste2</td>
+                  <td>teste2</td>
+                </tr>
+                <tr>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                </tr>
+                <tr>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                </tr>
+                <tr>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                </tr>
+                <tr>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                </tr>
+                <tr>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                </tr>
+                <tr>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                </tr>
+                <tr>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                </tr>
+                <tr>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                </tr>
+                <tr>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                  <td>teste3</td>
+                </tr>
+                
+              </table>
+            </div>
+
+            <div class="footer-principal">
+              <h1 class="totalGasto">Total gasto no mês:</h1>
+              <h1 class="valorGrande">1.000,00</h1>
+            </div>
+        </section>
+    </main>
+  </body>
+</html>
---
+++ view/sobreNos.php
@@ -0,0 +1,77 @@
+    <img src="img/bg-gradiente1.png" alt="" class="bg-gradiente1 bg-SN1" />
+    <img src="img/bg-gradiente2.png" alt="" class="bg-gradiente2 bg-SN2" />
+   
+    <main>
+        <section class="sobreNos">
+            <img src="img/logoTEXT.png" alt="logo_anzen" class="logoSN"/>
+            <h1 class="anzen">ANZEN</h1>
+            <p class="doAlemao">Do Alemão</p>
+            <h1 class="finanzen">Fin<strong>anzen</strong></h1>
+            <p class="doPortugues">Do Português</p>
+            <h1 class="financas">Finanças</h1>
+            <p><strong>Anzen</strong> é um website, feito como trabalho de conclusão de curso, que tem como objetivo organizar as finanças de seus usuários.</p>
+            
+            <h1 class="situacaoProblema">SITUAÇÃO PROBLEMA</h1>
+            <p>Muitas pessoas não sabem lidar com suas finanças e acabam se descontrolando em seus gastos mensais, gastando até mesmo o que não podem e acabam endividadas. A ideia do projeto é auxiliar essas pessoas para poderem ter controle sobre seu dinheiro. 
+            </p>
+            <p>O Excel é uma ótima ferramenta para se tratar de questões financeiras, entretanto muitas pessoas não sabem utilizá-lo  para tal tarefa. Outro problema é a falta de organização, muitas pessoas não sabem a data em que pagou e/ou recebeu. O público alvo do site são pessoas comuns, que não possuem tempo. O projeto visa solucionar estes problemas aqui descritos.
+            </p>
+            <h1 class="justificativa">JUSTIFICATIVA</h1>
+            <p>Administrar suas finanças é essencial para economizar dinheiro e não adquirir novas dívidas. Porém, para o cidadão comum que não tem conhecimentos sobre finanças pessoais ou sobre ferramentas de controle mensal, isso pode se mostrar como um problema.
+            </p>
+            <p>Nesta plataforma que desenvolvemos, os usuários podem encontrar um ambiente amigável e de fácil entendimento, que os guia pelo processo de controle de entrada e saída do dinheiro mensal.
+            </p>
+
+            <h1 class="metodologia">METODOLOGIA</h1>
+            <div class="flex-box">
+                <div>
+                    <p>VSCODE</p>
+                </div>
+                <div>
+                    <p>HTML</p>
+                </div>
+                <div>
+                    <p>CSS</p>
+                </div>
+                <div>
+                    <p>JavaScrip</p>
+                </div>
+                <div>
+                    <p>PHP</p>
+                </div>
+                <div>
+                    <p>MySQL</p>
+                </div>
+                <div>
+                    <p>Figma</p>
+                </div>
+                <div>
+                    <p>Trello</p>
+                </div>
+                <div>
+                    <p>GitHub</p>
+                </div>
+                <div>
+                    <p>Google Meet</p>
+                </div>
+                <div>
+                    <p>GIMP</p>
+                </div>
+                <div>
+                    <p>Word</p>
+                </div>
+                <div>
+                    <p>Inkscape</p>
+                </div>
+                <div>
+                    <p>XAMPP</p>
+                </div>
+                <div>
+                    <p>Power Point</p>
+                </div>
+            </div>
+        </section>
+    </main>
+
+  </body>
+</html>
