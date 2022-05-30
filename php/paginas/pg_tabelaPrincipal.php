f2218a8798938f8b62711f50f775d9cbc1f7375e
Vinicio Shurman <48026024+vinicioShurman@users.noreply.github.com>
2022-05-30 14:05:39 -0300

    limpando

--- php/paginas/pg_tabelaPrincipal.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Página que o usuário vai entrar quando fizer login
-session_start();
-include('index.php');
-?>
-
-<h2>Olá, <?php echo $_SESSION['nome'];?></h2>
-<h2><a href="logout.php">Sair</a></h2>
