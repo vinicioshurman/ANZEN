558e139df2f18b8007c34e23c604a79045840318
Nathanaely <93411156+nathanaelymendes@users.noreply.github.com>
2022-06-02 19:38:06 -0300

    Update principal.php

--- view/principal.php
+++ view/principal.php
@@ -58,77 +58,7 @@
                   <td>teste1</td>
                   <td>teste1</td>
                 </tr>
-                <tr>
-                  <td>teste2</td>
-                  <td>teste2</td>
-                  <td>teste2</td>
-                  <td>teste2</td>
-                  <td>teste2</td>
-                </tr>
-                <tr>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                </tr>
-                <tr>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                </tr>
-                <tr>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                </tr>
-                <tr>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                </tr>
-                <tr>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                </tr>
-                <tr>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                </tr>
-                <tr>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                </tr>
-                <tr>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                </tr>
-                <tr>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                  <td>teste3</td>
-                </tr>
-                
+               
               </table>
             </div>
 
@@ -140,3 +70,42 @@
     </main>
   </body>
 </html>
+<!--   <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                    <th>Descrição</th>
+                    <th>Foto</th>
+                    <th>Valor</th>
+                    <th>Tipo</th>
+                    <th><button type="button" class="btn btn-light" onclick="window.location.href='index.php?action=imovelNovo'">Novo</button></th>
+                </tr>
+            </thead>
+            <tbody>
+           <?php
+                    $imoveis = call_user_func(array('ImovelController', 'listar'));
+
+                    if(isset($imoveis) && !empty($imoveis)){
+                        foreach($imoveis as $imovel){
+                            ?>
+                                <tr>
+                                    <td><?php echo $imovel->getDescricao(); ?></td>
+                                    <td><?php echo "<img src='model/img/". $imovel->getFoto() ."' alt='Foto de exibição' width='100px'/>"; ?></td>
+                                    <td><?php echo $imovel->getValor(); ?></td>
+                                    <td><?php echo $imovel->getTipo(); ?></td>
+                                    <td>
+                                        <button type="button" class="btn btn-primary" onclick="window.location.href='index.php?action=imovelEditar&id=<?php echo $imovel->getId();?>'">Editar</button>
+                                        <button type="button" class="btn btn-danger"  onclick="window.location.href='index.php?action=imovelExcluir&id=<?php echo $imovel->getId();?>'">Excluir</button>
+                                    </td>
+                                </tr>
+                            <?php
+                        }
+                    } else {
+                        ?>
+                            <tr>
+                                <td colspan="5">Nenhum registro encontrado</td>
+                            </tr>
+                        <?php
+                    }
+                ?>
+            </tbody>
+        </table> -->
