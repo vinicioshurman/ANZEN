aa8ee19359347b6afeaae6629788bd953bb3561e
Nathanaely <93411156+nathanaelymendes@users.noreply.github.com>
2022-06-02 19:36:11 -0300

    Update login.php

--- view/login.php
+++ view/login.php
@@ -2,15 +2,22 @@
         <h1>Login</h1>
         <form name="cadUsuario" id="cadUsuario" action="" method="post">
             <label>Usuário: </label> <br>
-            <input class="field" type="text" name="login" id="login" value="<?php echo isset($usuario)?$usuario->getLogin():'' ?>" />
+            <input class="field" type="text" name="login" id="login" value="" />
             <br>
             <label>Senha:</label> <br>
-            <input class="field" type="password" name="senha1" id="senha1" value="<?php echo isset($usuario)?$usuario->getSenha():'' ?>" />
+            <input class="field" type="password" name="senha" id="senha" value="" />
             <br>
             <br>
-            <input type="submit" name="btnCadastrar" id="btnCadastrar" value="Cadastrar" class="btn btn-color-green"/>
+            <input type="submit" name="btnLogar" id="btnLogar" value="Cadastrar" class="btn btn-color-green"/>
             </div>
         </form>
     </main>
   </body>
 </html>
+<?php
+    if(isset($_POST['btnLogar'])){
+       $_SESSION['logado']= call_user_func(array('UsuarioController','logar'));
+       $_SESSION['login']= $_POST['login'];
+        header('Location: index.php');
+    }
+?>
