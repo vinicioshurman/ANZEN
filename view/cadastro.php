1f53221f790e4b7562f2516088389b9a9b0b2990
Nathanaely <93411156+nathanaelymendes@users.noreply.github.com>
2022-06-02 19:35:37 -0300

    Update cadastro.php

--- view/cadastro.php
+++ view/cadastro.php
@@ -5,14 +5,21 @@
             <input class="field" type="text" name="login" id="login" value="<?php echo isset($usuario)?$usuario->getLogin():'' ?>" />
             <br>
             <label>Senha:</label> <br>
-            <input class="field" type="password" name="senha1" id="senha1" value="<?php echo isset($usuario)?$usuario->getSenha():'' ?>" />
+            <input class="field" type="password" name="senha" id="senha" value="<?php echo isset($usuario)?$usuario->getSenha():'' ?>" />
             <br>
             <label>Senha:</label> <br>
-            <input class="field" type="password" name="senha1" id="senha1" value="<?php echo isset($usuario)?$usuario->getSenha():'' ?>" />
+            <input class="field" type="password" name="senha" id="senha" value="<?php echo isset($usuario)?$usuario->getSenha():'' ?>" />
             <br>
             <input type="submit" name="btnCadastrar" id="btnCadastrar" value="Cadastrar" class="btn btn-color-green"/>
             </div>
+
         </form>
     </main>
   </body>
 </html>
+<?php
+    if(isset($_POST['btnCadastrar'])){
+        call_user_func(array('UsuarioController','cadastro'));
+        header('Location: index.php?action=principal');
+    }
+?>
